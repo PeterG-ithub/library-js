16a264ce2be1bf390011eb847b36d7c9bbda2674
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-25 10:36:47 -0700

    Add dialog modal for add book button

--- library.js
+++ library.js
@@ -1,5 +1,7 @@
+//data storage
 const myLibrary = [];
 
+//book constructor
 function Book(title, author, pages) {
   this.title = title
   this.author = author
@@ -9,16 +11,40 @@ function Book(title, author, pages) {
   }
 }
 
+//method to add books to library
 function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
+//method to display books
 function displayAllBooks() {
   for (let i = 0; i < myLibrary.length; i++) {
     console.log(myLibrary[i].info())
   }
 }
 
+//modal for add book button 
+const favDialog = document.getElementById("favDialog");
+const add_book = document.querySelector('.add-book');
+const title = favDialog.querySelector('.title');
+const author = favDialog.querySelector('.author');
+const pages = favDialog.querySelector('.pages');
+const outputBox = document.querySelector('output');
+const confirmBtn = favDialog.querySelector("#confirmBtn");
+add_book.addEventListener('click', ()=> {
+  favDialog.showModal();
+});
+
+confirmBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let newBook = new Book(title.value, author.value, pages.value)
+  addBookToLibrary(newBook)
+  displayAllBooks()
+  //console.log(newBook.info())
+})
+function bookModal() {
+  console.log("Hello! I am an alert box!!")
+}
 
 theHobbit = new Book('The Hobbit', 'J.R.R Tolkein', 295)
 addBookToLibrary(theHobbit)
